436061e7e53d2f128430bd88473b96043702784e
SaraivaArthur <arthurdfranca09@gmail.com>
2026-04-16 19:46:06 -0300

    atividade 9 do curso de java

---
+++ numeroParImpar.java
@@ -0,0 +1,10 @@
+public class numeroParImpar {
+	public static void main(String[] args) {
+		int numero = 11;
+		if (numero % 2 == 0) { // aqui ele verifica se é divisivel por dois;
+		    System.out.println("Par");
+		} else {
+		    System.out.println("Ímpar");
+		}
+	}
+}
