6640f60a1994dc2faaeee540a002d888a3485eb0
SaraivaArthur <arthurdfranca09@gmail.com>
2026-04-16 22:29:53 -0300

    atividade 14 do curso de java

---
+++ valoresNotas.java
@@ -0,0 +1,15 @@
+public class valoresNotas {
+	public static void main(String[] args) {
+		int notaa = 7;
+		int notab = 10;
+
+		if (notaa + notab / 2 > 6) {
+			System.out.println("Você foi aprovado!");
+		} else {
+			System.out.println("Você foi reprovado!");
+		}
+
+
+	}
+}
+
