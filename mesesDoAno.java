1fea4b3fdad1d432d5606b81a99e84786f83baa5
SaraivaArthur <arthurdfranca09@gmail.com>
2026-04-16 22:04:26 -0300

    atividade 12 do curso de java

---
+++ mesesDoAno.java
@@ -0,0 +1,60 @@
+public class mesesDoAno {
+	public static void main(String[] args) {
+		int mes = 5;
+
+		switch (mes) {
+
+			case 1:
+				System.out.println("Jan.");
+				break;
+
+			case 2:
+				System.out.println("Fev.");
+				break;
+
+			case 3:
+				System.out.println("Mar.");
+				break;
+
+			case 4:
+				System.out.println("Abr.");
+				break;
+
+			case 5:
+				System.out.println("Mai.");
+				break;
+
+			case 6:
+				System.out.println("Jun.");
+				break;
+
+			case 7:
+				System.out.println("Jul.");
+				break;
+
+			case 8:
+				System.out.println("Ago.");
+				break;
+
+			case 9:
+				System.out.println("Set.");
+				break;
+
+			case 10:
+				System.out.println("Out.");
+				break;
+
+			case 11:
+				System.out.println("Nov.");
+				break;
+
+			case 12:
+				System.out.println("Dez.");
+				break;
+
+			default:
+				System.out.println("Mês desconhecido!");
+
+		}
+	}
+}
