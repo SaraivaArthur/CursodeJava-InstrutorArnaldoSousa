7026e49e1f17c09a988cfda3a97a9ac8df2a9b32
SaraivaArthur <arthurdfranca09@gmail.com>
2026-04-17 23:25:47 -0300

    atividade 17 do curso de java

---
+++ exercicioVerificacao.java
@@ -0,0 +1,11 @@
+public class exercicioVerificacao {
+	public static void main(String[] args) {
+		int soma = 0;
+
+		for (int i = 0; i <= 10; i++) {
+			soma += i; // acumula
+		}
+
+		System.out.println("Soma final: " + soma);
+	}
+}
