63e5b1fc71a0edea15bc7a307a410ada504dd7f4
SaraivaArthur <arthurdfranca09@gmail.com>
2026-04-18 15:05:14 -0300

    atividade 18 do curso de java

---
+++ exercicioDeWhile.java
@@ -0,0 +1,9 @@
+public class exercicioDeWhile {
+	public static void main(String[] args) {
+    	int contadorDoces = 1;
+    	while (contadorDoces <= 3) {
+    		System.out.println(contadorDoces);
+    		contadorDoces++;
+    	}
+	}
+}
